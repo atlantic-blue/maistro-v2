6a24dce26c97553429c1c3270282e5174dfcdc00
juliantellez <4896851+juliantellez@users.noreply.github.com>
2026-01-11 09:02:31 +0000

    packages: ui

---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,13 @@
+export { Button, buttonVariants, type ButtonProps } from "./button";
+export { Input, type InputProps } from "./input";
+export { Label } from "./label";
+export { Textarea, type TextareaProps } from "./textarea";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./card";
+export { Spinner } from "./spinner";
